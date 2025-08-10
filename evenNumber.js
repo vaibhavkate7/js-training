5b452e4080929a42bf7038206a56e2e0ba1acb88
vaibhavkate7 <katevaibhav417@gmail.com>
2025-08-10 16:59:24 +0530

    fix: Updated even number program

--- evenNumber.js
+++ evenNumber.js
@@ -1,11 +1,15 @@
+// Write a program to find given number is even or not.
+
 function evenNumber(num) {
   if (typeof (num) == "number") {
     let remainder = num % 2;
     if (remainder == 0) {
-      console.log("given number is even")
+      console.log("Given number is even number.")
+    } else {
+      console.log("Given number is not even number.");
     }
   } else {
-    console.log("invalid number")
+    console.log("Given number is invalid number.")
   }
 }
 
