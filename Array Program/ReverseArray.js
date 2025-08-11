21c6d016746c9f092fd381167be6e7f009200d40
vaibhavkate7 <katevaibhav417@gmail.com>
2025-08-11 14:34:50 +0530

    feat:reverse Arry

---
+++ Array Program/ReverseArray.js
@@ -0,0 +1,15 @@
+function reverseArray(arr){
+  let start=0;
+  let end =arr.length-1;
+  while (start<end){
+    let temp= arr[start]
+    arr[start]=arr[end]
+    arr[end]=temp;
+    start++;
+    end--;
+
+  }
+  return arr;
+}
+  let number=[1,2,3,4,5,6];
+console.log(reverseArray(number));
