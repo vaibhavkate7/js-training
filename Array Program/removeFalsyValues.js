857d146048d9ac9c9a7f0e0cfef0d28232920eda
vaibhavkate7 <katevaibhav417@gmail.com>
2025-08-11 16:33:43 +0530

    feat:added falsy value

--- Array Program/removeFalsyValues.js
+++ Array Program/removeFalsyValues.js
@@ -2,10 +2,8 @@ let arr = [4, false, 0, 6, "", null, 3, undefined, 1, NaN, 9];
 let result = [];
 for (let i = 0; i < arr.length; i++) {
   let value = arr[i];
-  if (arr[i]){
-     result.push(value);
+  if (arr[i]) {
+    result.push(value);
   }
-   
 }
-
 console.log(result); 
